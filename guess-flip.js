de68f4c4e150481d814f9fd7f6076686afe2190b
Brendan Merritt <brm2000@live.unc.edu>
2022-02-15 15:10:25 -0500

    fixing error message for autograder

--- guess-flip.js
+++ guess-flip.js
@@ -2,18 +2,13 @@ import { flipACoin } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 var argv = minimist(process.argv.slice(2))
-argv['call']
+argv["call"]
 
 var err = false;
-var errMessage = console.error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
+var errMessage = console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 
-if (argv.call == undefined) {
+if (argv.call == undefined || (argv.call != "heads" && argv.call != "tails")) {
     errMessage;
     err = true;
 }
-else if (argv.call != "heads" && argv.call != "tails") {
-    errMessage;
-    err = true;
-}
-
 if (err == false) { console.log(flipACoin(argv.call)); }
